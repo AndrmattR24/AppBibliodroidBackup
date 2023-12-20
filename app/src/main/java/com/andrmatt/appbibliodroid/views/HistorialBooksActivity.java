07b266997007b752cd7700e3c682c45fe3e184f3
AndrmattR24 <andrewmatiasrengifo@gmail.com>
2023-12-20 13:07:56 -0500

    view HistoryRoomDb

---
+++ app/src/main/java/com/andrmatt/appbibliodroid/views/HistorialBooksActivity.java
@@ -0,0 +1,16 @@
+package com.andrmatt.appbibliodroid.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.andrmatt.appbibliodroid.R;
+
+public class HistorialBooksActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_historial_books);
+    }
+}
